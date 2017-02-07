7c7fa2f12a91813d40c8d7d1a93e49d3eecae647
Mariano <mmontori@umich.edu>
2017-02-07 16:19:55 -0500

    start of project

---
+++ Scripts/test_movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_movement : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		Debug.Log (rb.velocity.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
