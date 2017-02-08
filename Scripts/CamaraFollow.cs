140ca2f91cbacd8baef9180548f00455a2a9dc25
Mariano <mmontori@umich.edu>
2017-02-07 22:52:24 -0500

    Add Movement
    -Movement added
    -Modified physics
    -Modified terrain

---
+++ Scripts/CamaraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 offset = new Vector3(0, -3, 5);
+	private Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+		mainCamera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		mainCamera.transform.position = player.transform.position - offset;
+		mainCamera.transform.LookAt(player.transform);
+//		Debug.Log (player.transform.position - offset);
+	}
+}
--- Scripts/test_movement.cs
+++ Scripts/test_movement.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class test_movement : MonoBehaviour {
 
+	public GameObject player;
+	public GameObject follow;
 	private Rigidbody rb;
+	public float speed;
+	public float playerRotation;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
-		Debug.Log (rb.velocity.y);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		Vector3 difference = follow.transform.position - player.transform.position;
+		Vector3 direction = new Vector3(Input.GetAxis("Vertical") * speed * difference.x, 
+			rb.velocity.y, Input.GetAxis("Vertical") * speed * difference.z);
+		player.transform.Rotate(Vector3.up * Input.GetAxis ("Horizontal") * 2f);
+		if(Input.GetAxisRaw("Vertical") != 0){
+			rb.velocity = direction;
+		}
+		else{
+			rb.velocity = Vector3.Slerp (rb.velocity, new Vector3(0, rb.velocity.y, 0), 1);
+		}
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Debug.Log ("WHAT");
+			rb.AddForce(0, 30, 0, ForceMode.Impulse);
+		}
+//		else{
+//			rb.velocity = rb.velocity;
+//		}
+
+//		rb.velocity = speed * Input.GetAxis("Vertical") * 
+
+
+
+//		Debug.Log (Input.GetAxis("Vertical") * Vector3.MoveTowards (player.transform.position, follow.transform.position, 1f));
+//		Debug.Log(follow.transform.position);
+//		player.transform.position = Input.GetAxis("Vertical") * Vector3.MoveTowards (player.transform.position, 
+//			follow.transform.position, 10f);
+//		follow.transform.position = new Vector3(5 * Mathf.Sin(Input.GetAxis("Horizontal")), 
+//			follow.transform.position.y, 5 * Mathf.Cos(Input.GetAxis("Horizontal")));
+//		speed = Input.GetAxis("Vertical") * 20;
+//		playerRotation = player.transform.rotation.y + Input.GetAxis("Horizontal");
+//		rb.velocity = new Vector3(speed * Input.GetAxis("Horizontal"), rb.velocity.y, speed * Input.GetAxis("Vertical"));
+//		if(Input.GetAxisRaw("Horizontal") != 0){
+//			player.transform.rotation = new Quaternion (player.transform.rotation.x, 
+//				player.transform.rotation.y * Mathf.PI / 180 + Input.GetAxis ("Horizontal") * Mathf.PI/2, 
+//				player.transform.rotation.z, player.transform.rotation.w);
+//		}
+//		else{
+//			player.transform.rotation = new Quaternion (player.transform.rotation.x, 
+//				player.transform.rotation.y, player.transform.rotation.z, player.transform.rotation.w);
+//		}
+//		playerRotation = 0;
+//
+//		Debug.Log (player.transform.rotation.y + Input.GetAxis("Horizontal"));
 	}
 }
