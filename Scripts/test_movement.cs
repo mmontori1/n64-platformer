e4174912c2ea57d74705e8b5f85d5a859abe0b62
Mariano <mmontori@umich.edu>
2017-02-23 01:42:16 -0500

    Switch to Tank Game
    -added notes
    -added limited jumping
    -changed model of the player (tank)
    -further improved axis mobility
    -tank rotates and follows the direction of movement
    -utilizing top-down camera view
    -bunch of commented code

--- Scripts/test_movement.cs
+++ Scripts/test_movement.cs
@@ -6,6 +6,8 @@ public class test_movement : MonoBehaviour {
 
 	public GameObject player;
 	public GameObject follow;
+	public GameObject cannon;
+	public Camera camera;
 	private Rigidbody rb;
 	public float speed;
 	public bool grounded;
@@ -24,10 +26,30 @@ public class test_movement : MonoBehaviour {
 		else{
 			speed = 10;
 		}
+//		Vector3 vec_1 = new Vector3 (-1, 0, 0);
+//		Vector3 vec_2 = new Vector3(player.transform.position.x + Input.GetAxis ("Mouse X"), 0, 
+//			player.transform.position.z + Input.GetAxis("Mouse Y"));
+//		print (Mathf.Pow(player.transform.position.x + Input.GetAxis ("Mouse X"),2) + 
+//			Mathf.Pow(player.transform.position.z + Input.GetAxis("Mouse Y"),2));
+//		Vector3 vec_2 = camera.ScreenToWorldPoint(Input.mousePosition);
+//		float angle = Mathf.Acos(vec_1.magnitude * vec_2.magnitude);
+//		Debug.Log(Input.mousePosition);
+//		Debug.Log (vec_2);
 		// calculates where the follow object should be
-		float distPosX = Input.GetAxis("Horizontal") / Mathf.Sqrt(1 + Mathf.Abs(Input.GetAxis("Vertical")));
-		float distPosZ = Input.GetAxis("Vertical") / Mathf.Sqrt(1 + Mathf.Abs(Input.GetAxis("Horizontal")));
-		follow.transform.localPosition = new Vector3 (distPosX, 0, distPosZ);
+		if(Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0){
+			follow.transform.localPosition = new Vector3 (Input.GetAxis ("Horizontal"), 0,
+				Input.GetAxis ("Vertical")).normalized;
+		}
+//		float angle = Input.GetAxisRaw("Horizontal") + Input.GetAxisRaw("Vertical");
+//		if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
+		float flip = Input.GetAxis("Vertical") > 0 ? -1 : 1;
+			float angle = flip * Vector2.Angle(new Vector2(1, 0),
+			new Vector2(follow.transform.localPosition.x, follow.transform.localPosition.z));
+//			float angle = 360 * (Mathf.Atan2(follow.transform.position.y, follow.transform.position.x)) / (Mathf.PI * 2);
+//			float angle = Vector3.Angle (origin, follow.transform.localPosition);
+			cannon.transform.eulerAngles = new Vector3(0, angle, 0);
+//		}
+//		Vector3.RotateTowards (player.transform.position, follow.transform.position, Time.deltaTime, 0);
 
 		// calculates velocity by comparing the difference between the object
 		Vector3 difference = follow.transform.position - player.transform.position;
@@ -42,12 +64,21 @@ public class test_movement : MonoBehaviour {
 		}
 
 		// space => jump
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if(Input.GetKeyDown(KeyCode.Space) && grounded){
 			rb.AddForce(0, 30, 0, ForceMode.Impulse);
 		}
 	}
 
 //  TO DO!
-//	void OnCollisionEnter(){
-//	}
+	void OnCollisionEnter(Collision coll){
+		if(coll.gameObject.tag == "ground"){
+			grounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision coll){
+		if(coll.gameObject.tag == "ground"){
+			grounded = false;
+		}
+	}
 }
