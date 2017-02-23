68c85ea591b2ffd9c90ace7292f6897c6de6f10d
Mariano <mmontori@umich.edu>
2017-02-23 02:13:08 -0500

    code cleanup
    -removed unwanted variable
    -improved readability
    -added comments/notes (to do)
    -edited camera perspective
    -changed some variables public => private

--- Scripts/test_movement.cs
+++ Scripts/test_movement.cs
@@ -7,49 +7,39 @@ public class test_movement : MonoBehaviour {
 	public GameObject player;
 	public GameObject follow;
 	public GameObject cannon;
-	public Camera camera;
 	private Rigidbody rb;
-	public float speed;
-	public bool grounded;
+	private float speed;
+	private bool grounded;
+	private Vector2 angleOrigin;
 
 	// Use this for initialization
 	void Start () {
+		angleOrigin = new Vector2(1, 0);
 		grounded = true;
 		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.LeftShift)){
-			speed = 15;
-		}
-		else{
-			speed = 10;
-		}
-//		Vector3 vec_1 = new Vector3 (-1, 0, 0);
-//		Vector3 vec_2 = new Vector3(player.transform.position.x + Input.GetAxis ("Mouse X"), 0, 
-//			player.transform.position.z + Input.GetAxis("Mouse Y"));
-//		print (Mathf.Pow(player.transform.position.x + Input.GetAxis ("Mouse X"),2) + 
-//			Mathf.Pow(player.transform.position.z + Input.GetAxis("Mouse Y"),2));
-//		Vector3 vec_2 = camera.ScreenToWorldPoint(Input.mousePosition);
-//		float angle = Mathf.Acos(vec_1.magnitude * vec_2.magnitude);
-//		Debug.Log(Input.mousePosition);
-//		Debug.Log (vec_2);
+		//shifts speed depending on whether left shift is pressed or not
+		speed = Input.GetKey(KeyCode.LeftShift) ? 15 : 10;
+			
 		// calculates where the follow object should be
 		if(Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0){
 			follow.transform.localPosition = new Vector3 (Input.GetAxis ("Horizontal"), 0,
 				Input.GetAxis ("Vertical")).normalized;
 		}
-//		float angle = Input.GetAxisRaw("Horizontal") + Input.GetAxisRaw("Vertical");
-//		if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
-		float flip = Input.GetAxis("Vertical") > 0 ? -1 : 1;
-			float angle = flip * Vector2.Angle(new Vector2(1, 0),
-			new Vector2(follow.transform.localPosition.x, follow.transform.localPosition.z));
-//			float angle = 360 * (Mathf.Atan2(follow.transform.position.y, follow.transform.position.x)) / (Mathf.PI * 2);
-//			float angle = Vector3.Angle (origin, follow.transform.localPosition);
+
+		// rotates tank cannon accordingly
+		if(Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0){
+			float flip = Input.GetAxis("Vertical") > 0 ? -1 : 1;
+			Vector2 angleRotate = new Vector2 (follow.transform.localPosition.x, follow.transform.localPosition.z);
+			float angle = flip * Vector2.Angle(angleOrigin, angleRotate);
 			cannon.transform.eulerAngles = new Vector3(0, angle, 0);
-//		}
-//		Vector3.RotateTowards (player.transform.position, follow.transform.position, Time.deltaTime, 0);
+		}
+		else{
+			cannon.transform.eulerAngles = new Vector3(0, cannon.transform.eulerAngles.y, 0);
+		}
 
 		// calculates velocity by comparing the difference between the object
 		Vector3 difference = follow.transform.position - player.transform.position;
@@ -69,7 +59,7 @@ public class test_movement : MonoBehaviour {
 		}
 	}
 
-//  TO DO!
+	//ground check for jumping
 	void OnCollisionEnter(Collision coll){
 		if(coll.gameObject.tag == "ground"){
 			grounded = true;
