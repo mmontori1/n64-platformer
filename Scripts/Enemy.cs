aaf82a87290518f4b6a0e334728c22c78fcab243
Mariano <mmontori@umich.edu>
2017-03-01 17:25:08 -0500

    Added Enemy movement
    -Enemy now moves towards the player using NavMeshAgent

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 
+	public NavMeshAgent agent;
+	public GameObject player;
+	public Rigidbody rb;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start(){
+		agent = GetComponent<NavMeshAgent>();
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate(){
+		if(Mathf.Abs(this.transform.position.y - player.transform.position.y) < 1){
+			agent.SetDestination(player.transform.position);
+		}
+		else{
+			agent.SetDestination (Vector3.zero);
+		}
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if(coll.gameObject.tag == "bouncer"){
+			print ("WOW");
+			agent.Stop(true);
+			rb.AddRelativeForce(0, 30f, 0, ForceMode.Impulse);
+			agent.Resume();
+		}
 	}
 }
