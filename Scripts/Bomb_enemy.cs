63d0ddaa0bf5b36e42abca41092e72fb8d213b54
Mariano <mmontori@umich.edu>
2017-03-07 20:41:04 -0500

    Added new bomb enemy

--- Scripts/Bomb_enemy.cs
+++ Scripts/Bomb_enemy.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour {
+public class Bomb_enemy : MonoBehaviour {
 
 	public NavMeshAgent agent;
 	public GameObject player;
 	public Rigidbody rb;
+	private Vector3 origin;
+	public bouncer platform;
 
 	// Use this for initialization
 	void Start(){
 		agent = GetComponent<NavMeshAgent>();
 		rb = GetComponent<Rigidbody>();
+		origin = this.transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate(){
-		if(Mathf.Abs(this.transform.position.y - player.transform.position.y) < 1){
+		if(platform.contact){
+			print (player.transform.position);
 			agent.SetDestination(player.transform.position);
 		}
 		else{
-			agent.SetDestination(Vector3.zero);
+			agent.SetDestination(origin);
 		}
 	}
 
 	void OnCollisionEnter(Collision coll){
-		if(coll.gameObject.CompareTag("bouncer")){
-			print ("WOW");
-			agent.Stop();
-			rb.AddRelativeForce(0, 30f, 0, ForceMode.Impulse);
-			agent.Resume();
+		if(coll.gameObject.CompareTag("Player")){
 		}
 	}
 }
---
+++ Scripts/bouncer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouncer : MonoBehaviour {
+
+	public bool contact;
+
+	// Use this for initialization
+	void Start () {
+		contact = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if(coll.gameObject.CompareTag("Player")){
+			contact = true;
+		}
+	}
+
+	void OnCollisionExit(Collision coll){
+		if(coll.gameObject.CompareTag("Player")){
+			contact = false;
+		}
+	}
+}
